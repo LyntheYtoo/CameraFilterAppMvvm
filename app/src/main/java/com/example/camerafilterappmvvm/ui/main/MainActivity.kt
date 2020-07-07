441dd121caa89b036cef481b6582f5dfddc080a8
LyntheYtoo <cdwdong1@gmail.com>
2020-07-07 17:26:22 +0900

    Add MVVM으로 구조 변경
    ViewModel에서 카메라 관련 메인로직을 처리하도록 변경
    Activity에서는 뷰모델 초기화와 퍼미션 관련 작업만 함

--- app/src/main/java/com/example/camerafilterappmvvm/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/camerafilterappmvvm/ui/main/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.SurfaceHolder
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.example.camerafilterappmvvm.R
 import com.example.camerafilterappmvvm.imageProcess.CameraApi
 import com.example.camerafilterappmvvm.model.CameraParams
@@ -14,13 +16,21 @@ import com.example.camerafilterappmvvm.util.showToast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: MainViewModel
+    lateinit var cameraPreviewCallback: SurfaceHolder.Callback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 뷰모델 초기화 및 뷰모델관련 작업
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        cameraPreviewCallback = viewModel.createCameraPreviewCallback(this, preview_camera.holder.surface)
+
+        // 퍼미션 체크
         if(!checkPermissions(this, NECESSARY_PERMISSIONS))
             askPermissions(this, NECESSARY_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-
-        preview_camera.holder.addCallback(cameraPreviewCallback)
+        else preview_camera.holder.addCallback(cameraPreviewCallback)
 
         // Hide the status bar
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
@@ -33,38 +43,25 @@ class MainActivity : AppCompatActivity() {
                 )
     }
 
+    /**
+     * 퍼미션 결과 처리
+     */
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // 이 액티비티에서 요청한 퍼미션이고
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (checkPermissions(this, NECESSARY_PERMISSIONS))
-                CameraApi.openCamera(this, CameraParams(preview_camera.holder.surface, 0))
+            // 모든 퍼미션이 허락된 상황이라면
+            if (checkPermissions(this, NECESSARY_PERMISSIONS)) {
+                // 카메라를 키고 프리뷰의 뷰에 카메라 관련 콜백을 부착
+                viewModel.openCamera(this, preview_camera.holder.surface)
+                preview_camera.holder.addCallback(cameraPreviewCallback)
+            }
             else showToast(this, "퍼미션이 거부되었습니다.")
         }
-    }
-
-    private val cameraPreviewCallback = object : SurfaceHolder.Callback {
-        override fun surfaceChanged(
-            holder: SurfaceHolder?,
-            format: Int,
-            width: Int,
-            height: Int) {
-
-        }
-
-        override fun surfaceDestroyed(holder: SurfaceHolder?) {
-            CameraApi.closeCamera()
-        }
-
-        override fun surfaceCreated(holder: SurfaceHolder?) {
-            if (!checkPermissions(this@MainActivity, NECESSARY_PERMISSIONS)) return
-            CameraApi.openCamera(this@MainActivity,
-                CameraParams(preview_camera.holder.surface, 0))
-        }
-
     }
 
     companion object {
--- app/src/main/java/com/example/camerafilterappmvvm/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/camerafilterappmvvm/ui/main/MainViewModel.kt
@@ -1,7 +1,44 @@
 package com.example.camerafilterappmvvm.ui.main
 
+import android.content.Context
+import android.view.Surface
+import android.view.SurfaceHolder
 import androidx.lifecycle.ViewModel
+import com.example.camerafilterappmvvm.imageProcess.CameraApi
+import com.example.camerafilterappmvvm.model.CameraParams
 
 class MainViewModel : ViewModel() {
+    private val cameraApi = CameraApi
 
+    private var cameraPosition: Int = 0
+
+    /**
+     * SurfaceView 에 등록할 카메라프리뷰 콜백을 만들어주는 함수
+     */
+    fun createCameraPreviewCallback(context: Context, preview: Surface) = object : SurfaceHolder.Callback {
+        override fun surfaceChanged(
+            holder: SurfaceHolder?,
+            format: Int,
+            width: Int,
+            height: Int
+        ) {}
+
+        override fun surfaceDestroyed(holder: SurfaceHolder?) {
+            CameraApi.closeCamera()
+        }
+
+        override fun surfaceCreated(holder: SurfaceHolder?) {
+            CameraApi.openCamera(
+                context,
+                CameraParams(preview, 0)
+            )
+        }
+    }
+
+    /**
+     * 카메라 관련 동작
+     */
+    fun openCamera(context: Context, surface: Surface) =
+        cameraApi.openCamera(context, CameraParams(surface, cameraPosition))
+    fun closeCamera() = cameraApi.closeCamera()
 }
